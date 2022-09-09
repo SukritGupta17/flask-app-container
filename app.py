8b90f1b750f0e5908b52371fc13695987f48edfd
Sukrit Gupta <sukrit.g17@gmail.com>
2022-09-09 12:08:55 +1000

    Add logger for debugging

--- app.py
+++ app.py
@@ -2,31 +2,40 @@ import os
 
 from flask import Flask
 import sqlalchemy
+import logging
 
 from connect_connector import connect_with_connector
 
 app = Flask(__name__)
+logger = logging.getLogger()
 
 
 def init_connection_pool() -> sqlalchemy.engine.base.Engine:
 
     # use the connector when INSTANCE_CONNECTION_NAME is defined
     if os.environ.get("INSTANCE_CONNECTION_NAME"):
+        logger.info("connecting using python connector")
         return connect_with_connector()
-
+    logger.info("failed to connect")
     raise ValueError(
         "Missing database connection type. Please define INSTANCE_CONNECTION_NAME"
     )
 
 
+@app.before_first_request
+def init_db() -> sqlalchemy.engine.base.Engine:
+    global db
+    db = init_connection_pool()
+
+
 @app.route("/home")
 def home():
-    db = init_connection_pool()
     with db.connect() as conn:
         # Execute the query and fetch all results
         all_planets = conn.execute(
             "SELECT * FROM planets"
         ).fetchall()
+    logger.info(f"all planets fetched {all_planets}")
     return "<p>Hello, World!</p> \n Welcome home. \n"
 
 
