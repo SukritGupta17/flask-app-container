7c122663829017ce400ddabdee0c38fa7688b9a4
Sukrit Gupta <sukrit.g17@gmail.com>
2022-09-09 10:40:48 +1000

    add cloud sql connection using python connector

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 import os
 
 from flask import Flask
+import sqlalchemy
+
+from connect_connector import connect_with_connector
 
 app = Flask(__name__)
 
+
+def init_connection_pool() -> sqlalchemy.engine.base.Engine:
+
+    # use the connector when INSTANCE_CONNECTION_NAME is defined
+    if os.environ.get("INSTANCE_CONNECTION_NAME"):
+        return connect_with_connector()
+
+    raise ValueError(
+        "Missing database connection type. Please define INSTANCE_CONNECTION_NAME"
+    )
+
+
 @app.route("/home")
 def home():
-	return "<p>Hello, World!</p> \n Welcome home."
+    db = init_connection_pool()
+    with db.connect() as conn:
+        # Execute the query and fetch all results
+        all_planets = conn.execute(
+            "SELECT * FROM planets"
+        ).fetchall()
+    return f"<p>Hello, World!</p> \n Welcome home. \n <p>{all_planets}</p>"
+
 
 if __name__ == "__main__":
-	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
